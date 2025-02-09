e76873c807ea53a0ad702de79fba09e00066e7a1
zinntikumugai <uesitananame55@gmail.com>
2025-02-09 22:14:22 +0900

    i2c scan sample

---
+++ README.md
@@ -0,0 +1,17 @@
+# RaspberryPi Pico + I2C Scan
+
+# Example 
+4device connected.
+```
+I2C Bus Scan
+INFO       0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+INFO  00   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
+INFO  10   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
+INFO  20   .   .   .   .   .   .   .  27   .   .   .   .   .   .   .   . 
+INFO  30   .   .   .   .   .   .   .   .  38   .   .   .  3C   .   .   . 
+INFO  40   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
+INFO  50   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
+INFO  60   .   .   .   .   .   .   .   .   .   .   .   .   .   .   .   . 
+INFO  70   .   .   .   .   .   .   .  77   .   .   .   .   .   .   .   . 
+INFO  Scan End.
+```
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@
 
 use defmt::*;
 use defmt_rtt as _;
+use embedded_hal::{delay::DelayNs, i2c::I2c};
 use panic_probe as _;
 use rp2040_hal as hal;
 
 use hal::pac;
 
-use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin;
+use fugit::RateExtU32;  // ファイルの先頭に追加
+use core::fmt::Write;  // Write トレイトをアンダースコアでインポート
+use heapless::String;
+
 
 #[link_section = ".boot2"]
 #[used]
@@ -47,28 +50,50 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut green_led = pins.gpio22.into_push_pull_output();
-    let mut orange_led = pins.gpio21.into_push_pull_output();
-    let mut red_led = pins.gpio20.into_push_pull_output();
+
+    let sda_pin = pins.gpio16.into_pull_up_input().into_function::<hal::gpio::FunctionI2C>();
+    let scl_pin = pins.gpio17.into_pull_up_input().into_function::<hal::gpio::FunctionI2C>();
+    
+    // I2C0 の初期化（400kHz）
+    let mut i2c = hal::I2C::i2c0(
+        pac.I2C0,
+        sda_pin,
+        scl_pin,
+        400_000u32.Hz(),
+        &mut pac.RESETS,
+        &clocks.system_clock,
+    );
 
     loop {
-        info!("green");
-        green_led.set_high().unwrap();
         timer.delay_ms(2000);
-        green_led.set_low().unwrap();
 
-        info!("orange");
-        for _ in 1..4 {
-            orange_led.set_high().unwrap();
-            timer.delay_ms(500);
-            orange_led.set_low().unwrap();
-            timer.delay_ms(500);
+        info!("\nI2C Bus Scan");
+        info!("     0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F");
+
+        let mut buf: String<256> = String::new();
+        let mut count = 0;
+    
+        for addr in 0..=127u8 {
+    
+            let mut data = [0u8; 1];
+            let result = i2c.read(addr, &mut data);
+            
+            match result {
+                Ok(_) => {
+                    let _ = core::write!(&mut buf, " {:02X} ", addr);
+                },
+                Err(_) => {
+                    let _ = core::write!(&mut buf, "  . ");
+                },
+            }
+
+            if addr % 16 == 15 {
+                info!("{}0 {=str}", count, buf.as_str());
+                buf = String::new();
+                count += 1;
+            }
         }
-        orange_led.set_low().unwrap();
 
-        info!("red");
-        red_led.set_high().unwrap();
-        timer.delay_ms(2000);
-        red_led.set_low().unwrap();
+        info!("Scan End.")
     }
 }
